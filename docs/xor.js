1d1316b4ddd791c838a4a30d16f2e712b4ffa7b1
188w <107601386+188w@users.noreply.github.com>
2022-12-13 17:40:39 +0000

    Fixed missing imports

--- docs/xor.js
+++ docs/xor.js
@@ -1,1 +1,12 @@
-impo
+import { BPNet, Matrix, toFixed } from './infers.esm.js'
+
+const container = document.getElementById('container')
+const canvas = document.getElementById('canvas')
+const buts = document.querySelectorAll('#buts button')
+const rateNode = document.getElementById('rate')
+const modeNode = document.getElementById('mode')
+
+const ctx = canvas.getContext('2d')
+let [W, H] = [container.getBoundingClientRect().width, 500]
+
+const model = new BPNet([2, [6
