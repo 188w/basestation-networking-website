66fec10353601b6d824f461337b1e1d192060b3e
188w <107601386+188w@users.noreply.github.com>
2022-12-18 07:30:33 +0000

    improvements.

--- docs/xor.js
+++ docs/xor.js
@@ -9,4 +9,19 @@ const modeNode = document.getElementById('mode')
 const ctx = canvas.getContext('2d')
 let [W, H] = [container.getBoundingClientRect().width, 500]
 
-const model = new BPNet([2, [6
+const model = new BPNet([2, [6, 'Tanh'], [6, 'Tanh'], [2, 'Softmax']], { rate: Number(rateNode.value) })
+let xs = new Matrix([[1, 0], [0, 1], [0, 0], [1, 1]])
+let ys = new Matrix([[1, 0], [1, 0], [0, 1], [0, 1]])
+
+rateNode.oninput = v => {
+  model.rate = Number(v.target.value)
+}
+
+modeNode.onchange = v => {
+  model.mode = v.target.value
+}
+
+function init() {
+  W = container.getBoundingClientRect().width
+  canvas.width = W
+  can
