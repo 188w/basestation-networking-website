b34ffb6c765636576a73af501bfe16fd844cba7d
188w <107601386+188w@users.noreply.github.com>
2023-01-14 09:04:26 +0000

    Update fields

--- example/xor.ts
+++ example/xor.ts
@@ -3,4 +3,11 @@ import { Matrix, BPNet } from '../src'
 export function xor() {
   let xs = new Matrix([[1, 0], [0, 1], [0, 0], [1, 1]])
   let ys = new Matrix([[1, 0], [1, 0], [0, 1], [0, 1]])
-  let model = new BPNet([2, [6, 'Tanh'], [6, 'Tanh'], [2, 'Softmax
+  let model = new BPNet([2, [6, 'Tanh'], [6, 'Tanh'], [2, 'Softmax']], { rate: 0.01, mode: 'sgd' })
+  model.fit(xs, ys, {
+    epochs: 2000, onEpoch: (epoch, loss) => {
+      if (epoch % 100 === 0) console.log('epoch:' + epoch, 'loss:', loss)
+    }
+  })
+  model.predict(xs).print()
+}
---
+++ lib/BPNet.d.ts
@@ -0,0 +1,3 @@
+import { Matrix } from "./matrix";
+import type { TrainingOptions, Mode, BPNetOptions, NetShape } from "./types";
+export declare const defaultTrainingOptions: (m: number) => Trai
