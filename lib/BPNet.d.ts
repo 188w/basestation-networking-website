bb83a5e49c4c4cc472cb782ea54ac55e624aa94c
188w <107601386+188w@users.noreply.github.com>
2023-01-14 19:47:22 +0000

    incremented version

--- lib/BPNet.d.ts
+++ lib/BPNet.d.ts
@@ -1,3 +1,17 @@
 import { Matrix } from "./matrix";
 import type { TrainingOptions, Mode, BPNetOptions, NetShape } from "./types";
-export declare const defaultTrainingOptions: (m: number) => Trai
+export declare const defaultTrainingOptions: (m: number) => TrainingOptions;
+export declare class BPNet {
+    readonly shape: NetShape;
+    w: Matrix[];
+    b: Matrix[];
+    hlayer: number;
+    scale?: Matrix;
+    mode: Mode;
+    rate: number;
+    constructor(shape: NetShape, opt?: BPNetOptions);
+    unit(l: number): number;
+    af(l: number): import("./types").ActivationFunction | undefined;
+    toJSON(): string;
+    static fromJSON(json: string): BPNet;
+    forwardPropagation(xs: Ma
