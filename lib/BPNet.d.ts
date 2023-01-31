b2ec42b8f087a0839835ec65d2aa12aa951b5b9c
188w <107601386+188w@users.noreply.github.com>
2023-01-31 07:19:16 +0000

    Corrected syntax error

--- lib/BPNet.d.ts
+++ lib/BPNet.d.ts
@@ -14,4 +14,17 @@ export declare class BPNet {
     af(l: number): import("./types").ActivationFunction | undefined;
     toJSON(): string;
     static fromJSON(json: string): BPNet;
-    forwardPropagation(xs: Ma
+    forwardPropagation(xs: Matrix): Matrix[];
+    scaled(xs: Matrix): Matrix;
+    predict(xs: Matrix): Matrix;
+    predictNet(xs: Matrix): Matrix[];
+    backPropagation(hy: Matrix[], xs: Matrix, ys: Matrix): {
+        dy: Matrix[];
+        dw: Matrix[];
+    };
+    calcBackPropagation(hy: Matrix[], xs: Matrix, ys: Matrix): {
+        dy: Matrix[];
+        dw: Matrix[];
+    };
+    adjust(dy: Matrix[], dw: Matrix[]): void;
+    cost(hy: M
