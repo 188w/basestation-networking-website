e8b946805e48731a8e003c61ea35f2e17e576ce9
188w <107601386+188w@users.noreply.github.com>
2023-02-09 14:59:23 +0000

    Update some deps

---
+++ lib/matrix.d.ts
@@ -0,0 +1,39 @@
+
+export interface GenerateMatrixOptions {
+    range: [number, number];
+    integer?: boolean;
+}
+export declare class Matrix {
+    shape: [number, number];
+    private self;
+    constructor(data: number[][]);
+    slice(start: number, end: number): Matrix;
+    argMax(row: number): number;
+    connect(b: Matrix): Matrix;
+    zeroed(): Matrix;
+    clone(): Matrix;
+    getMeanOfRow(row: number): number;
+    sum(): number;
+    columnSum(): Matrix;
+    dataSync(): number[][];
+    equalsShape(b: Matrix): boolean;
+    equals(b: Matrix): boolean;
+    static generate(row: number, col: number, opt?: GenerateMatrixOptions | number): Matrix;
+    update(row: number, col: number, val: number, oper?: '+=' | '-=' | '*=' | '/='): void;
+    expand(n: number, position: 'L' | 'R' | 'T' | 'B'): Matrix;
+    get(row: number, col: number): number;
+    getRow(row: number): number[];
+    getCol(col: number): number[];
+    adjugate(): Matrix;
+    inverse(): Matrix;
+    det(): number;
+    cominor(row: number, col: number): Matrix;
+    atomicOperation(callback: (item: number, row: number, col: number) => number): Matrix;
+    coLocationOperation(b: Matrix, oper: 'add' | 'sub' | 'mul' | 'exp'): Matrix;
+    subtraction(b: Matrix): Matrix;
+    addition(b: Matrix): Matrix;
+    multiply(b: Matrix | number): Matrix;
+    get T(): Matrix;
+    normalization(): Matrix[];
+    print(): void;
+}
