0fd47c29a5df3855354da2caf200de40f1e9e33e
188w <107601386+188w@users.noreply.github.com>
2023-04-09 09:48:18 +0000

    Run update-remote.sh on openjdk

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,3 +1,16 @@
 import { Matrix } from "./matrix";
 export declare type ActivationFunction = 'Sigmoid' | 'Relu' | 'Tanh' | 'Softmax';
-export declare type Mode
+export declare type Mode = 'sgd' | 'bgd' | 'mbgd';
+export declare type NetShape = [number, (number | [number, ActivationFunction]), ...(number | [number, ActivationFunction])[]];
+export interface BPNetOptions {
+    mode?: Mode;
+    rate?: number;
+    w?: Matrix[];
+    b?: Matrix[];
+    scale?: Matrix;
+}
+export interface TrainingOptions {
+    epochs: number;
+    batchSize: number;
+    async: boolean;
+    onBatch?: (batch: number, size: number, loss: numbe
