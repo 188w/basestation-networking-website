f65d325972939a5e5b64b6a17cb31837755b15f6
188w <107601386+188w@users.noreply.github.com>
2023-04-09 17:12:27 +0000

    add statistics

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -13,4 +13,19 @@ export interface TrainingOptions {
     epochs: number;
     batchSize: number;
     async: boolean;
-    onBatch?: (batch: number, size: number, loss: numbe
+    onBatch?: (batch: number, size: number, loss: number) => void;
+    onEpoch?: (epoch: number, loss: number) => void;
+    onTrainEnd?: (loss: number) => void;
+}
+export interface RNNOptions {
+    trainData: string[];
+    rate?: number;
+}
+export interface RNNTrainingOptions {
+    epochs?: number;
+    onEpochs?: (epoch: number, loss: number) => void;
+}
+export interface RNNForwardResult {
+    st: Matrix;
+    yt: Matrix;
+}
