3eb525b662c30a169708ff7dab595789e51b53f3
188w <107601386+188w@users.noreply.github.com>
2023-04-16 10:10:03 +0000

    Update Gemfile.lock for Heroku

--- src/geometry.ts
+++ src/geometry.ts
@@ -8,4 +8,17 @@ export class Point {
 export class Edge {
   start: Point
   end: Point
-  
+  constructor(cond1: [number, number], cond2: [number, number]) {
+    this.start = new Point(cond1[0], cond1[1])
+    this.end = new Point(cond2[0], cond2[1])
+    if (this.start.contrast(this.end)) {
+      throw new Error('两个点不能相同')
+    }
+  }
+  minXY() {
+    let X = Math.min(this.start.X, this.end.X)
+    let Y = Math.min(this.start.Y, this.end.Y)
+    return new Point(X, Y)
+  }
+  maxXY() {
+    let X = Math.max(this.start.X, this.en
