070f7c36232918f38cd400229c21bbd58dc58486
188w <107601386+188w@users.noreply.github.com>
2023-04-29 10:16:25 +0000

    PHP

--- src/matrix.ts
+++ src/matrix.ts
@@ -12,4 +12,28 @@ export class Matrix {
     let t = data.find((d, i) => data[i - 1] && d.length !== data[i - 1].length)
     if (t) throw new Error('Matrix column inconsistent')
     if (!data[0].length) throw new Error('Matrix has at least one element from row')
-    this.shape
+    this.shape = [data.length, data[0].length]
+    this.self = data
+  }
+
+  /**
+   * 上下分割矩阵
+   */
+  slice(start: number, end: number) {
+    return new Matrix(this.self.slice(start, end))
+  }
+
+  /**
+   * 返回行的最大值索引
+   */
+  argMax(row: number) {
+    let d = this.getRow(row)
+    let max = d[0]
+    let index = 0
+    for (let i = 0; i < d.length; i++) {
+      if (d[i] > max) {
+        max = d[i]
+        index = i
+      }
+    }
+ 
