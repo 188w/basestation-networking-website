674215951d963424b3665068c83477bb94d4317d
188w <107601386+188w@users.noreply.github.com>
2023-04-29 06:18:40 +0000

    add upload js

--- src/matrix.ts
+++ src/matrix.ts
@@ -1,2 +1,15 @@
 export interface GenerateMatrixOptions {
-  range: [number
+  range: [number, number],
+  integer?: boolean
+}
+
+export class Matrix {
+  shape: [number, number]
+  private self: number[][]
+
+  constructor(data: number[][]) {
+    if (!data[0]) throw new Error('Matrix at least one row')
+    let t = data.find((d, i) => data[i - 1] && d.length !== data[i - 1].length)
+    if (t) throw new Error('Matrix column inconsistent')
+    if (!data[0].length) throw new Error('Matrix has at least one element from row')
+    this.shape
